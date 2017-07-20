f75ea2907d1241ad1b3e22e7a78070e0be056366
Ethan Fung <EthanEFung@gmail.com>
2017-07-20 15:59:02 -0700

    Develop index.js

--- server/index.js
+++ server/index.js
@@ -4,4 +4,10 @@ const port = 3000;
 
 const ip = '127.0.0.1';
 
-const server = http.createServer((request, response) => {})
+const server = http.createServer((request, response) => {
+  console.log(`serving request type ${request.method} for url ${request.url}`);
+})
+
+console.log(`listening on port ${port}`);
+
+server.listen(port, ip);
