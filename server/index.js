e26875a495183a91d281ca0002f02a10ddca9408
Ethan Fung <EthanEFung@gmail.com>
2017-07-20 16:05:12 -0700

    declare routes

--- server/index.js
+++ server/index.js
@@ -1,13 +1,15 @@
 const http = require('http');
-
+const routes = require('./requestRoutes');
 const port = 3000;
+const ip = '127.0.0.1' || 'localhost';
 
-const ip = '127.0.0.1';
+const router = {
+  '/': routes.static, //root page usually pages like html or css
+
+};
 
 const server = http.createServer((request, response) => {
   console.log(`serving request type ${request.method} for url ${request.url}`);
-})
-
+});
 console.log(`listening on port ${port}`);
-
 server.listen(port, ip);
